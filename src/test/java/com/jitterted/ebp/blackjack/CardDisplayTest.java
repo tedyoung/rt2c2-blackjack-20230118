e7aefedf6b73866c806205904c8dce7686533f92
Ted M. Young <tedyoung@gmail.com>
2023-01-18 11:10:15 -0800

    LAB #1: Characterize Card: display non-Ten as string

--- src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -13,4 +13,12 @@ class CardDisplayTest {
         assertThat(card.display())
                 .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
     }
+
+    @Test
+    void displayNonTenAsString() {
+        Card card = new Card(Suit.SPADES, Rank.QUEEN);
+
+        assertThat(card.display())
+                .isEqualTo("[30m┌─────────┐[1B[11D│Q        │[1B[11D│         │[1B[11D│    ♠    │[1B[11D│         │[1B[11D│        Q│[1B[11D└─────────┘");
+    }
 }
