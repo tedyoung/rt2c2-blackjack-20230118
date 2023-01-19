a1ff32aa1ee669c07f0ef7d816ff90fa23980466
Ted M. Young <tedyoung@gmail.com>
2023-01-19 11:58:09 -0800

    LAB #4: Test for Player Stands with better hand, and beats the Dealer

--- src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -19,6 +19,20 @@ class GameOutcomeTest {
         assertThat(game.determineOutcome())
                 .isEqualTo("You Busted, so you lose.  💸");
     }
+
+    @Test
+    void playerDealtBetterHandThanDealerAndStandsThenPlayerBeatsDealer() {
+        Deck playerStandsAndBeatsDealer = new StubDeck(Rank.TEN, Rank.EIGHT,
+                                                       Rank.QUEEN, Rank.JACK);
+        Game game = new Game(playerStandsAndBeatsDealer);
+        game.initialDeal();
+
+        game.playerStands();
+        game.dealerTurn();
+
+        assertThat(game.determineOutcome())
+                .isEqualTo("You beat the Dealer! 💵");
+    }
 }
 
 
