956157f4c497315e274c4f9f97929221001fc1b3
Ted M. Young <tedyoung@gmail.com>
2023-01-19 12:11:44 -0800

    LAB #4: Extracted enum: GameOutcome and updated usages of determineOutcome

---
+++ src/main/java/com/jitterted/ebp/blackjack/domain/GameOutcome.java
@@ -0,0 +1,18 @@
+package com.jitterted.ebp.blackjack.domain;
+
+public enum GameOutcome {
+    PLAYER_BUSTED("You Busted, so you lose.  💸"),
+    DEALER_BUSTED("Dealer went BUST, Player wins! Yay for you!! 💵"),
+    PLAYER_BEATS_DEALER("You beat the Dealer! 💵"),
+    PLAYER_PUSHES("Push: Nobody wins, we'll call it even."),
+    PLAYER_LOSES("You lost to the Dealer. 💸");
+    private final String display;
+
+    public String displayString() {
+        return display;
+    }
+
+    GameOutcome(String display) {
+        this.display = display;
+    }
+}
