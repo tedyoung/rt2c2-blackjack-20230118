72e1378181fc46bd5a133ce31552d02d897226f8
Ted M. Young <tedyoung@gmail.com>
2023-01-19 08:50:16 -0800

    DAY 2 STARTER: Initial split of Game and ConsoleGame to kick-start the creation of a Console-specific Adapter.

--- src/main/java/com/jitterted/ebp/blackjack/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/Game.java
@@ -14,6 +14,8 @@ public class Game {
     private final Hand dealerHand = new Hand();
     private final Hand playerHand = new Hand();
 
+    private boolean playerDone;
+
     public static void main(String[] args) {
         displayWelcomeScreen();
         waitForEnterFromUser();
@@ -23,7 +25,7 @@ public class Game {
         resetScreen();
     }
 
-    private static void resetScreen() {
+    public static void resetScreen() {
         System.out.println(ansi().reset());
     }
 
@@ -33,7 +35,7 @@ public class Game {
         game.play();
     }
 
-    private static void waitForEnterFromUser() {
+    public static void waitForEnterFromUser() {
         System.out.println(ansi()
                                    .cursor(3, 1)
                                    .fgBrightBlack().a("Hit [ENTER] to start..."));
@@ -41,7 +43,7 @@ public class Game {
         System.console().readLine();
     }
 
-    private static void displayWelcomeScreen() {
+    public static void displayWelcomeScreen() {
         AnsiConsole.systemInstall();
         System.out.println(ansi()
                                    .bgBright(Ansi.Color.WHITE)
@@ -77,7 +79,7 @@ public class Game {
         dealerHand.drawFrom(deck);
     }
 
-    private void determineOutcome() {
+    public void determineOutcome() {
         if (playerHand.isBusted()) {
             System.out.println("You Busted, so you lose.  💸");
         } else if (dealerHand.isBusted()) {
@@ -91,7 +93,7 @@ public class Game {
         }
     }
 
-    private void dealerTurn() {
+    public void dealerTurn() {
         // Dealer makes its choice automatically based on a simple heuristic (<=16 must hit, =>17 must stand)
         if (!playerHand.isBusted()) {
             while (dealerHand.dealerMustDrawCard()) {
@@ -120,13 +122,13 @@ public class Game {
         }
     }
 
-    private String inputFromPlayer() {
+    public String inputFromPlayer() {
         System.out.println("[H]it or [S]tand?");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
-    private void displayGameState() {
+    public void displayGameState() {
         System.out.print(ansi().eraseScreen().cursor(1, 1));
         System.out.println("Dealer has: ");
         System.out.println(ConsoleHand.displayFaceUpCard(dealerHand));
@@ -140,7 +142,19 @@ public class Game {
         System.out.println(" (" + playerHand.value() + ")");
     }
 
-    private void displayBackOfCard() {
+    public void displayFinalGameState() {
+        System.out.print(ansi().eraseScreen().cursor(1, 1));
+        System.out.println("Dealer has: ");
+        System.out.println(ConsoleHand.cardsAsString(dealerHand));
+        System.out.println(" (" + dealerHand.value() + ")");
+
+        System.out.println();
+        System.out.println("Player has: ");
+        System.out.println(ConsoleHand.cardsAsString(playerHand));
+        System.out.println(" (" + playerHand.value() + ")");
+    }
+
+    public void displayBackOfCard() {
         System.out.print(
                 ansi()
                         .cursorUp(7)
@@ -154,16 +168,18 @@ public class Game {
                         .a("└─────────┘"));
     }
 
-    private void displayFinalGameState() {
-        System.out.print(ansi().eraseScreen().cursor(1, 1));
-        System.out.println("Dealer has: ");
-        System.out.println(ConsoleHand.cardsAsString(dealerHand));
-        System.out.println(" (" + dealerHand.value() + ")");
 
-        System.out.println();
-        System.out.println("Player has: ");
-        System.out.println(ConsoleHand.cardsAsString(playerHand));
-        System.out.println(" (" + playerHand.value() + ")");
+    public void playerHits() {
+        playerHand.drawFrom(deck);
+        playerDone = playerHand.isBusted();
+    }
+
+    public void playerStands() {
+        playerDone = true;
+    }
+
+    public boolean isPlayerDone() {
+        return playerDone;
     }
 
 }
