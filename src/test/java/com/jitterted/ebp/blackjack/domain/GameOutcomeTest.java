3b4e1a44e368e7d3068c73371fe9fd66a3172728
Ted M. Young <tedyoung@gmail.com>
2023-01-19 11:53:11 -0800

    LAB #4: Add test for player hits and goes bust, with configurable Stub Deck

--- src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -1,12 +1,17 @@
 package com.jitterted.ebp.blackjack.domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 
 class GameOutcomeTest {
 
-    //    @Test
+    @Test
     void playerHitsAndGoesBustThenOutcomeIsPlayerLoses() {
-        Game game = new Game(new ShuffledDeck());
+        Deck playerHitsAndGoesBustDeck = new StubDeck(Rank.TEN, Rank.EIGHT,
+                                                      Rank.QUEEN, Rank.JACK,
+                                                      Rank.THREE);
+        Game game = new Game(playerHitsAndGoesBustDeck);
         game.initialDeal();
 
         game.playerHits();
