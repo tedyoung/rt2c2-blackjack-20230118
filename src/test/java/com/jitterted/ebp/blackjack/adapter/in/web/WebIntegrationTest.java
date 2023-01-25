b43dfd223051cd064ddd2ea307b2666ccf78bcbb
Ted M. Young <tedyoung@gmail.com>
2023-01-25 10:56:13 -0800

    TDD: GET to /game endpoint works

--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/WebIntegrationTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/WebIntegrationTest.java
@@ -27,4 +27,10 @@ public class WebIntegrationTest {
                .andExpect(status().is3xxRedirection());
     }
 
+    @Test
+    public void getRequestToGameIsStatus200Ok() throws Exception {
+        mockMvc.perform(get("/game"))
+               .andExpect(status().isOk());
+    }
+
 }
